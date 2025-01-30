eadc0acd0fd4ec286f18c033d5ad32beb78a8a5f
PeDitX <pedram.ale@me.com>
2025-01-30 06:50:50 +0330

    Update ezp.sh

--- ezp.sh
+++ ezp.sh
@@ -58,31 +58,47 @@ sleep 2
 opkg install whiptail
 sleep 2
 
-# GitHub repository URL and package name
-REPO_URL="https://github.com/peditx/luci-theme-peditx"
-LATEST_RELEASE_URL="https://api.github.com/repos/peditx/luci-theme-peditx/releases/latest"
-IPK_URL=$(curl -s $LATEST_RELEASE_URL | grep "browser_download_url.*ipk" | cut -d '"' -f 4)
+# Function to install a theme
+install_theme() {
+  local REPO_NAME=$1
+  local THEME_NAME=$2
 
-# Check if the download link is found
-if [ -z "$IPK_URL" ]; then
-  echo "Download link for the .ipk file not found."
-  exit 1
-fi
+  echo "Processing $THEME_NAME..."
 
-# Download the .ipk package
-echo "Downloading the latest version of luci-theme-peditx..."
-wget -q $IPK_URL -O /tmp/luci-theme-peditx.ipk
+  # GitHub repository URL and package name
+  LATEST_RELEASE_URL="https://api.github.com/repos/peditx/$REPO_NAME/releases/latest"
+  IPK_URL=$(curl -s $LATEST_RELEASE_URL | grep "browser_download_url.*ipk" | cut -d '"' -f 4)
 
-# Install the .ipk package
-echo "Installing luci-theme-peditx..."
-opkg install /tmp/luci-theme-peditx.ipk
+  # Check if the download link is found
+  if [ -z "$IPK_URL" ]; then
+    echo "Download link for the .ipk file of $THEME_NAME not found."
+    return 1
+  fi
 
-# Clean up the downloaded file
-rm /tmp/luci-theme-peditx.ipk
+  # Download the .ipk package
+  echo "Downloading the latest version of $THEME_NAME..."
+  wget -q $IPK_URL -O /tmp/$THEME_NAME.ipk
 
+  # Install the .ipk package
+  echo "Installing $THEME_NAME..."
+  opkg install /tmp/$THEME_NAME.ipk
+
+  # Clean up the downloaded file
+  rm /tmp/$THEME_NAME.ipk
+
+  echo "$THEME_NAME installed successfully."
+}
+
+# Install luci-theme-peditx
+install_theme "luci-theme-peditx" "luci-theme-peditx"
+
+# Install luci-theme-carbonpx
+install_theme "luci-theme-carbonpx" "luci-theme-carbonpx"
+
+opkg remove luci-theme-bootstrap --force-depends
 # Restart the web service to apply the changes
+echo "Restarting uhttpd service to apply changes..."
 /etc/init.d/uhttpd restart
-
 
 clear
 
